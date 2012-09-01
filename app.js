926e340a0894b40417cdc3aa81afdf026874a9db
PJ Hoberman <pj.hoberman@gmail.com>
2012-08-31 18:34:35 -0600

    it works. host it.

--- app.js
+++ app.js
@@ -10,14 +10,17 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+var active_users = [];
 io.sockets.on('connection', function (client) {
-
 
     client.on('join', function (name) {
         client.set('name', name);
         client.emit('message', {from: 'bot', message: 'Welcome to the chat bot, ' + name});
         client.broadcast.emit('message', {from: 'bot', message: name + ' has joined.'});
         client.broadcast.emit('join', name);
+        active_users.push(name);
+        active_users.sort();
+        client.emit('active_users', active_users);
         storage.lrange('messages', 0, -1, function (err, messages) {
             for (var i in messages) {
                 client.emit('message', JSON.parse(messages[i]));
@@ -29,6 +32,11 @@ io.sockets.on('connection', function (client) {
         client.get('name', function (error, name) {
             client.broadcast.emit('message', {from: 'bot', message: name + ' has left.'});
             client.broadcast.emit('leave', name);
+            for (var i in active_users) {
+                if (active_users[i] === name){
+                    active_users.splice(i, 1);
+                }
+            }
         });
     });
 
