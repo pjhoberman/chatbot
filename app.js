3b5d1a4c4a897105aeb7535a3d6edb3d26e39c5d
PJ Hoberman <pj.hoberman@gmail.com>
2012-09-14 16:58:26 -0600

    jslint

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server)
-  , fs = require('fs');
+/*jslint node:true */
+/*global require, process */
+
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    fs = require('fs');
 
 var port = process.env.PORT || 3000;
 
 server.listen(port);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+    res.sendfile(__dirname + '/index.html');
 });
 
 var active_users = [];
@@ -24,8 +27,9 @@ io.sockets.on('connection', function (client) {
         client.emit('active_users', active_users);
         fs.readFile('log.js', "utf8", function (err, data) {
             if (!err) {
-                var msgs = JSON.parse(data);
-                for(var i=0; i<msgs.length; i++){
+                var msgs = JSON.parse(data),
+                    i = 0;
+                for (i; i < msgs.length; i++) {
                     client.emit('message', msgs[i]);
                 }
             }
@@ -36,8 +40,9 @@ io.sockets.on('connection', function (client) {
         client.get('name', function (error, name) {
             client.broadcast.emit('message', {from: 'bot', message: name + ' has left.'});
             client.broadcast.emit('leave', name);
-            for (var i in active_users) {
-                if (active_users[i] === name){
+            var i;
+            for (i in active_users) {
+                if (active_users[i] === name) {
                     active_users.splice(i, 1);
                 }
             }
@@ -48,13 +53,13 @@ io.sockets.on('connection', function (client) {
         client.get('name', function (error, name) {
             var msg = {from: name, message: message};
             client.broadcast.emit('message', msg);
-            fs.readFile('log.js', encoding='utf8', function(err, data){
-                if(err){
-                    if(err.code !== "ENOENT") {
+            fs.readFile('log.js', 'utf8', function (err, data) {
+                if (err) {
+                    if (err.code !== "ENOENT") {
                         console.log('err');
                     } else {
-                        fs.writeFile('log.js', '[' + JSON.stringify(msg) + ']', encoding='utf8', function(err){
-                            if(err){
+                        fs.writeFile('log.js', '[' + JSON.stringify(msg) + ']', 'utf8', function (err) {
+                            if (err) {
                                 console.log(err);
                             } else {
                                 console.log('we good');
@@ -68,13 +73,13 @@ io.sockets.on('connection', function (client) {
                     d.push(msg);
                     console.log(d);
 
-                    fs.writeFile('log.js', JSON.stringify(d), encoding='utf8', function(err){
-                        if(err){
+                    fs.writeFile('log.js', JSON.stringify(d), 'utf8', function (err) {
+                        if (err) {
                             console.log(err);
                         } else {
                             console.log('good');
                         }
-                    })
+                    });
                 }
             });
         });
